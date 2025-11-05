28e23c1d31f1537fbac55e546bc89aba0dfb20ba
ItsMeNengYi <20040408gry@gmail.com>
2025-11-06 03:23:13 +0800

    Small changes

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from tqdm import tqdm
 from utils import data_previewer, DataObject, preprocessing_remove_lines, preprocessing_split_by_characters, process_character_image
 from model import Model, infer, check_correctness
 
-keys = list(string.ascii_lowercase) + [str(i) for i in range(10)]
-char_dict = {k: [] for k in keys}
 char_dir = 'chars_train_data/'
 os.makedirs(char_dir, exist_ok=True)
 
@@ -44,23 +42,13 @@ def main():
                     predictions = infer(net, img)
 
                     images_count += 1
-                    if check_correctness(predictions, ground_truth) == False:
+                    if check_correctness(predictions, ground_truth, with_allowance=True) == False:
                         wrong_count += 1
                         if len(ground_truth) == len(char_images):
                             data.append(DataObject(img, processed_img, num_chars, char_images, ground_truth, predictions))
                         continue
 
-                    # Correct separated
-                    for i in range(len(ground_truth)):
-                        k = ground_truth[i]
-                        char_dict[k].append(char_images[i])
-         # --- 4. Save each image to PNG ---
-        for k, img_list in char_dict.items():
-            for idx, img in enumerate(img_list):
-                filename = f"{k}_{idx:05d}.png"  # e.g., a_0000.png
-                path = os.path.join(char_dir, filename)
-                cv2.imwrite(path, img)
-        print(f"Wrong count: {wrong_count}")
+        # print(f"Wrong count: {wrong_count}")
     data_previewer(st.session_state.data)
 
 
